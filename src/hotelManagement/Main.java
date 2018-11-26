58af5e257e70e033ac7daba07321f1afeb6456a2
Ludvig <ludvigjonsson28@gmail.com>
2018-11-26 10:48:17 +0100

    ny test

--- src/hotelManagement/Main.java
+++ src/hotelManagement/Main.java
@@ -3,5 +3,6 @@ package hotelManagement;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello friends...!???????");
+        System.out.println("hajhaj");
     }
 }
