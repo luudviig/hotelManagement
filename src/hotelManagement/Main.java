a13c9bbb696629468c727614eb3966d3a2cf9857
Kristian Katona <kristiankatona1@gmail.com>
2018-11-26 10:44:07 +0100

    Tjenaregit status!

--- src/hotelManagement/Main.java
+++ src/hotelManagement/Main.java
@@ -2,6 +2,6 @@ package hotelManagement;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello friends...");
+        System.out.println("Hello friends...!???????");
     }
 }
