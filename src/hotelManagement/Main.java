07344c4c2ba0d343265697782ff2c1b57788f755
Kristian Katona <kristiankatona1@gmail.com>
2018-12-03 13:21:11 +0100

    ny Customer

--- src/hotelManagement/Main.java
+++ src/hotelManagement/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello friends...!???????");
         System.out.println("hajhaj");
+
+
+        Customer cust1 = new Customer("9907238822", "name", "kyrkovägen 3", "0700435678", "apa@hotmail.com");
+
     }
 }
