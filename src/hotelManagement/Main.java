53372423f3e8f763045ba796443a804cebe935a1
Ludvig <ludvigjonsson28@gmail.com>
2018-12-03 11:40:46 +0100

    lagt till class Room

--- src/hotelManagement/Main.java
+++ src/hotelManagement/Main.java
@@ -1,9 +1,48 @@
 package hotelManagement;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello friends...!???????");
-        System.out.println("hajhaj");
-        System.out.println("Massage received over");
+        Scanner input = new Scanner(System.in);
+        Main getMethod = new Main();
+
+        //Detta är en array som består av flera obejkt(Room)-Ludde
+        Room[] hotelRooms = new Room[100];
+
+        //Skapar en loop som lägger till objekt(Room), första våningen-Ludde
+        for (int i = 0; i<25; i++){
+            Room room1 = new Room(i+1,2,false,2000,false,1);
+            hotelRooms[i] = room1;
+        }
+        //Skapar en loop som lägger till objekt(Room), andra våningen-Ludde
+        for (int i = 25; i<50; i++){
+            Room room2 = new Room(i+1,2,true,2500,false,2);
+            hotelRooms[i] = room2;
+        }
+        //Skapar en loop som lägger till objekt(Room), tredje våningen-Ludde
+        for (int i = 49; i<100; i++){
+            Room room3 = new Room(i+1,2,true,3000,false,3);
+            hotelRooms[i] = room3;
+        }
+
+        //Ett sätt för att se vad som finns i array(hotelRooms).
+//        for (int i = 0; i<hotelRooms.length; i++){
+//            System.out.println("Room number: " + hotelRooms[i].getRoomNumber());
+//            System.out.println("Number of beds: " + hotelRooms[i].getNumberOfBeds());
+//            System.out.println("Price per night: " + hotelRooms[i].getPricePerNight());
+//            System.out.println("Booked: " + hotelRooms[i].isBooked());
+//            System.out.println("Balcony: " + hotelRooms[i].isHasBalcony());
+//            System.out.println();
+//        }
+
+
+
+
     }
+
+//    private Room[] makeBooking(Room room, Room[] hotelRooms,int roomToBook) {
+//
+//    }
 }
