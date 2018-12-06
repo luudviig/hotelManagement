cbd32682d50937a24bb62af03f3bdb09930bc536
Ludvig <ludvigjonsson28@gmail.com>
2018-12-06 10:52:08 +0100

    Lagt till ny klass med alla metoder istället för main, + arraylist

--- src/hotelManagement/PrintMenus.java
+++ src/hotelManagement/PrintMenus.java
@@ -1,7 +1,7 @@
 package hotelManagement;
 
 public class PrintMenus {
-    public static void viewmenucustom() {
+    public static void viewMenuCustom() {
 
         System.out.println("------Menu-----");
         System.out.println("[1] View available room");
@@ -11,12 +11,12 @@ public class PrintMenus {
         System.out.println("[5] Exit");
     }
 
-    public static void viewmenu() {
+    public static void viewMenu() {
         System.out.println("------Menu-----");
         System.out.println("[1] View customer");
         System.out.println("[2] View rooms");
         System.out.println("[3] View available room");
-        System.out.println("[4] Add booking");
+        System.out.println("[4] Add room");
         System.out.println("[5] Edit room information");
         System.out.println("[6] Remove room");
         System.out.println("[7] Add customer");
