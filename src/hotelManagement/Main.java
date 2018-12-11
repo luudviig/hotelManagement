67803b784a7779aa2e6e1c4ca743a5ce04c91087
Ludvig <ludvigjonsson28@gmail.com>
2018-12-11 09:53:21 +0100

    ny merge

--- src/hotelManagement/Main.java
+++ src/hotelManagement/Main.java
@@ -20,45 +20,5 @@ public class Main {
         //kallar på metod som skapar alla rum.
         hotelRooms = hotelLogic.createArrayListOfRooms(hotelRooms);
 
-        //skapar en loop som loopar tills man "exitar"-Ludde
-        do {
-            PrintMenus.viewMenu();
-            int choice = Integer.parseInt(getMethod.input.nextLine());
-            switch (choice) {
-                case 1:
-                    hotelLogic.viewCustomer(arrayListCustomer);
-                    break;
-                case 2:
-                    hotelLogic.viewRoom(hotelRooms);
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    hotelRooms = hotelLogic.addRoom(hotelRooms);
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    arrayListCustomer = hotelLogic.addCustomer(arrayListCustomer);
-                    break;
-                case 8:
-                    arrayListCustomer = hotelLogic.removeCustomer(arrayListCustomer, bookings);
-                    break;
-                case 9:
-                    hotelLogic.viewBookings(bookings);
-                    break;
-                case 10:
-                    bookings = hotelLogic.makeBooking(bookings, arrayListCustomer, hotelRooms);
-                    break;
-                case 11:
-                    hotelLogic.viewBookings(bookings);
-                    break;
-                default:
-                    System.out.println("Incorrect input, enter a number between 1-10");
-                    break;
-            }
-        } while (a);
     }
 }
