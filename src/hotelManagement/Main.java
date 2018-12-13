ab7775232a8bf188b60aca9897c21e838618ece5
Abdifatah <zafex188@hotmail.com>
2018-12-13 10:23:47 +0100

    Added default in switch

--- src/hotelManagement/Main.java
+++ src/hotelManagement/Main.java
@@ -26,109 +26,113 @@ public class Main {
 
 
 
-        //skapar en loop som loopar tills man "exitar"-Ludde
-        do {
-            PrintMenus.printMenu();
-            int answer = Integer.parseInt(input.nextLine());
-            b=true;
-            switch (answer) {
-                case 1:
-                    do { c=true;
-                        PrintMenus.viewMenuEmployeer1();
-                        int choise1 = Integer.parseInt(input.nextLine());
-                        if (choise1==1) {
-                            do {
-                                PrintMenus.viewMenuEmployerOption1();
-                                int choise2 = Integer.parseInt(input.nextLine());
-                                if (choise2 == 1) {
-                                    hotelLogic.viewCustomer(arrayListCustomer);
-                                } else if (choise2 == 2) {
-                                    arrayListCustomer = hotelLogic.addCustomer(arrayListCustomer);
-                                } else if (choise2 == 3) {
-                                    arrayListCustomer = hotelLogic.removeCustomer(arrayListCustomer, bookings);
-                                } else if (choise2 == 4) {
-                                    arrayListCustomer = hotelLogic.editCustomInfo(arrayListCustomer);
 
-                                } else if (choise2 == 5) {
-                                    //View current bookings for specific customer
-                                } else if (choise2 == 6) {
-                                    //History of all bookings specific customer
-                                } else if (choise2 == 7) {
-                                    hotelLogic.viewInfoAboutCustomer(arrayListCustomer,bookings);
-                                } else if (choise2 == 8){
-                                    c=false;
-                                }
-                            } while (c);
-                        }
-                        else if (choise1==2){
-                            do {
-                                PrintMenus.viewMenuEmployerOption2();
-                                int choise4 = Integer.parseInt(input.nextLine());
-                                if (choise4 == 1) {
-                                    hotelLogic.viewRoom(hotelRooms);
-                                } else if (choise4 == 2) {
-                                    //view available rooms
-                                } else if (choise4 == 3) {
-                                    hotelRooms = hotelLogic.addRoom(hotelRooms);
-                                } else if (choise4 == 4) {
-                                    //remove room
-                                } else if (choise4 == 5) {
-                                    //edit room information
-                                    hotelRooms = hotelLogic.editRoom(hotelRooms);
-                                } else if (choise4 == 6) {
-                                    c = false;
-                                }
-                            }while (c);
-                        }
-                        else if (choise1==3){
-                            do {
-                                PrintMenus.viewMenuEmployerOption3();
-                                int choise2 = Integer.parseInt(input.nextLine());
 
-                                if (choise2 == 1) {
-                                    hotelLogic.viewBookings(bookings);
-                                } else if (choise2 == 2) {
-                                   bookings = hotelLogic.makeBooking(bookings,arrayListCustomer,hotelRooms);
-                                } else if (choise2 == 3) {
-                                    //remove booking
-                                } else if (choise2 == 4) {
-                                    hotelLogic.searchBooking(bookings);
-                                } else if (choise2 == 5) {
+            //skapar en loop som loopar tills man "exitar"-Ludde
+            do {
+                PrintMenus.printMenu();
+                int answer = Integer.parseInt(input.nextLine());
+                b = true;
+                switch (answer) {
+                    case 1:
+                        do {
+                            c = true;
+                            PrintMenus.viewMenuEmployeer1();
+                            int choise1 = Integer.parseInt(input.nextLine());
+                            if (choise1 == 1) {
+                                do {
+                                    PrintMenus.viewMenuEmployerOption1();
+                                    int choise2 = Integer.parseInt(input.nextLine());
+                                    if (choise2 == 1) {
+                                        hotelLogic.viewCustomer(arrayListCustomer);
+                                    } else if (choise2 == 2) {
+                                        arrayListCustomer = hotelLogic.addCustomer(arrayListCustomer);
+                                    } else if (choise2 == 3) {
+                                        arrayListCustomer = hotelLogic.removeCustomer(arrayListCustomer, bookings);
+                                    } else if (choise2 == 4) {
+                                        arrayListCustomer = hotelLogic.editCustomInfo(arrayListCustomer);
 
-                                } else if (choise2 == 6) {
-                                    //check in customer
+                                    } else if (choise2 == 5) {
+                                        //View current bookings for specific customer
+                                    } else if (choise2 == 6) {
+                                        //History of all bookings specific customer
+                                    } else if (choise2 == 7) {
+                                        hotelLogic.viewInfoAboutCustomer(arrayListCustomer, bookings);
+                                    } else if (choise2 == 8) {
+                                        c = false;
+                                    }
+                                } while (c);
+                            } else if (choise1 == 2) {
+                                do {
+                                    PrintMenus.viewMenuEmployerOption2();
+                                    int choise4 = Integer.parseInt(input.nextLine());
+                                    if (choise4 == 1) {
+                                        hotelLogic.viewRoom(hotelRooms);
+                                    } else if (choise4 == 2) {
+                                        //view available rooms
+                                    } else if (choise4 == 3) {
+                                        hotelRooms = hotelLogic.addRoom(hotelRooms);
+                                    } else if (choise4 == 4) {
+                                        //remove room
+                                    } else if (choise4 == 5) {
+                                        //edit room information
+                                        hotelRooms = hotelLogic.editRoom(hotelRooms);
+                                    } else if (choise4 == 6) {
+                                        c = false;
+                                    }
+                                } while (c);
+                            } else if (choise1 == 3) {
+                                do {
+                                    PrintMenus.viewMenuEmployerOption3();
+                                    int choise2 = Integer.parseInt(input.nextLine());
 
-                                } else if (choise2 == 7) {
-                                    //check out customer
-                                }
-                                else if (choise2==8){
-                                    c=false;
-                                }
-                            } while (c);
-                        }
-                        else if (choise1==5){
-                            b=false;
-                        }
-                    }while (b);
-                    break;
-                case 2:
-                    PrintMenus.viewMenuCustomer();
-                    int choise2 = Integer.parseInt(input.nextLine());
-                    do {
-                        if (choise2 == 1) {
-                        } else if (choise2 == 2) {
-                        } else if (choise2 == 3) {
-                        } else if (choise2 == 4) {
-                        } else if (choise2 == 5) {
-                            b = false;
-                        }
-                    } while (b);
-                    break;
-                case 3:
-                    PrintMenus.printByeMessage();
-                    a=false;
-                    break;
-            }
-        } while(a);
+                                    if (choise2 == 1) {
+                                        hotelLogic.viewBookings(bookings);
+                                    } else if (choise2 == 2) {
+                                        bookings = hotelLogic.makeBooking(bookings, arrayListCustomer, hotelRooms);
+                                    } else if (choise2 == 3) {
+                                        //remove booking
+                                    } else if (choise2 == 4) {
+                                        hotelLogic.searchBooking(bookings);
+                                    } else if (choise2 == 5) {
+
+                                    } else if (choise2 == 6) {
+                                        //check in customer
+
+                                    } else if (choise2 == 7) {
+                                        //check out customer
+                                    } else if (choise2 == 8) {
+                                        c = false;
+                                    }
+                                } while (c);
+                            } else if (choise1 == 5) {
+                                b = false;
+                            }
+                        } while (b);
+                        break;
+                    case 2:
+                        PrintMenus.viewMenuCustomer();
+                        int choise2 = Integer.parseInt(input.nextLine());
+                        do {
+                            if (choise2 == 1) {
+                            } else if (choise2 == 2) {
+                            } else if (choise2 == 3) {
+                            } else if (choise2 == 4) {
+                            } else if (choise2 == 5) {
+                                b = false;
+                            }
+                        } while (b);
+                        break;
+                    case 3:
+                        PrintMenus.printByeMessage();
+                        a = false;
+                        break;
+
+                        default:
+                            System.out.println("Invalid choice");
+                            break;
+                }
+        } while (a);
+
     }
 }
